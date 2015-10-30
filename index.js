53a57741795ef7a7f274b59e1fc36979e44290b0
spencer-hanson <spencerhanson3141@gmail.com>
2015-10-30 01:57:02 -0600

    Re-organized project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
