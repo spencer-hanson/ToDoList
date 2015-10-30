7df4f15e65878999cf63e31a7ad55385e6e19beb
spencer-hanson <spencerhanson3141@gmail.com>
2015-10-30 01:10:39 -0600

    sdas

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var nohm = require('nohm').Nohm;
 if(process.env.REDISTOGO_URL) {
 	var rtg = require("url").parse(process.env.REDISTOGO_URL);
 	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	redis.auth(rtg.auth.split(":")[1]);
 } else {
 	var redis = require("redis").createClient();
 }
